af7a3b5aeefd6c0838fc91a92f3ea895b1a7cc74
hannes <hannesdelbeke@gmail.com>
2022-08-21 22:56:32 +0100

    rename/refactor sample code as unittest

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -15,25 +15,12 @@ class ValidateHelloWorld(Validator):
     def validate_instance(self, instance):
         assert instance == "Hello World"
 
-    # def _run(self):
-    #     assert instance == "Hello World"
 
-
-def main():
+def test_simple_session2():
     session = Session()
     session.registered_plugins.append(CollectHelloWorld)
     session.registered_plugins.append(CollectHelloWorldList)
     session.registered_plugins.append(ValidateHelloWorld)
     session.run()
 
-    print()
-    # # print validation results instances
-    # for plugin in session.plugin_instances:
-    #     print(plugin)
-    #     print(plugin.children)
-    #     for inst in plugin.children:
-    #         print('STATE', inst, inst.parent, inst.parent.state)  # TODO get validation state, atm we get parent(collect) state
-
-    session.pp_tree()
-if __name__ == '__main__':
-    main()
+    # wrap_a = session.plugin_instances[0].instance_wrappers[0]
